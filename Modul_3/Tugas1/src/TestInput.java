cb41b7ecc888f662a9837f3c8724007a886f4da0
zakelyk <candra.setyo.w@gmail.com>
2023-05-02 09:07:39 +0700

    add

---
+++ Modul_3/Tugas1/src/TestInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TestInput{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan Input : ");
+        String line = input.nextLine();
+
+
+    }
+}
